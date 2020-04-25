ad2f5c8b9745e906bd0c4d4c817accfb6a5a7bfd
rafa91seixas <rafaelaasseixas@gmail.com>
2020-04-25 18:51:09 +0000

    Favourite

--- mobdev-ca2/src/app/pages/episode-details/episode-details.page.ts
+++ mobdev-ca2/src/app/pages/episode-details/episode-details.page.ts
@@ -1,38 +1,35 @@
-import { FavouriteService } from '../../services/favourite.service';
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { FavouriteService } from '../../services/favourite.service';
 @Component({
-    selector: 'app-episode-details',
-    templateUrl: './episode-details.page.html',
-    styleUrls: ['./episode-details.page.scss'],
+  selector: 'app-episode-details',
+  templateUrl: './episode-details.page.html',
+  styleUrls: ['./episode-details.page.scss'],
 })
 export class EpisodeDetailsPage implements OnInit {
+  episode: any;
+  episodeId = null;
+  isFavourite = false;
 
-    episode: any;
-    isFavourite = false;
-    episodeId = null;
+  constructor(private activatedRoute: ActivatedRoute, 
+    private api: ApiService, 
+    private favouriteService: FavouriteService,
+    ) {}
 
-
-
-    constructor(private activatedRoute: ActivatedRoute,
-        private api: ApiService,
-        private favouriteService: FavouriteService,
-    ) { }
-
-    ngOnInit() {
-
+  ngOnInit() {
+        //page iniciation of the page
         this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
-
+        //asking Api to take the episode (where the http goes for that specific episode(id))
+        //pass the request back to Api
         this.api.getEpisode(this.episodeId).subscribe(res => {
-            this.episode = res[0];
+            this.episode = res[0]; //the result of that request for each id of this array
         });
 
 
 
         this.favouriteService.isFavourite(this.episodeId).then(isFav => {
-            this.isFavourite = isFav;
+            this.isFavourite = isFav; 
         });
     }
 
@@ -40,15 +37,15 @@ export class EpisodeDetailsPage implements OnInit {
 
     favouriteEpisode() {
         this.favouriteService.favouriteEpisode(this.episodeId).then(() => {
-            this.isFavourite = true;
+            this.isFavourite = true; 
         });
     }
 
     unfavouriteEpisode() {
         this.favouriteService.unfavouriteEpisode(this.episodeId).then(() => {
-            this.isFavourite = false;
+            this.isFavourite = false; 
         });
     }
-
-
+     
+    
 }
