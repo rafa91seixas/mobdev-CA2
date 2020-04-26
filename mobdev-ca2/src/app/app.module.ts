e03b4718c377410c896d72aa767d9827e227d4ee
rafa91seixas <rafaelaasseixas@gmail.com>
2020-04-26 19:53:36 +0000

    InfiniteScroll

--- mobdev-ca2/src/app/app.module.ts
+++ mobdev-ca2/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, IonInfiniteScroll } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,12 +10,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
+import { InfiniteScrollModule} from 'ngx-infinite-scroll';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot(), InfiniteScrollModule],
   providers: [
     StatusBar,
     SplashScreen,
--- mobdev-ca2/src/app/pages/episodes/episodes.page.ts
+++ mobdev-ca2/src/app/pages/episodes/episodes.page.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 @Component({
     selector: 'app-episodes',
     templateUrl: './episodes.page.html',
     styleUrls: ['./episodes.page.scss'],
 })
 export class EpisodesPage implements OnInit {
+    //@ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll
+
     episodes: Observable<any>;
     constructor(private router: Router, private api: ApiService) { }
     ngOnInit() {
@@ -24,5 +27,16 @@ export class EpisodesPage implements OnInit {
         this.router.navigateByUrl(`/tabs/episodes/${episodeId}`);
         console.log('my dataEpisodeDetails: ', episodeId);
     }
+    loadData(event) {
+        setTimeout(() => {
+            console.log('Done');
+            event.target.complete();
+
+            if (this.api.getQuote.length == 1000) {
+                event.target.disabled = true;
+            }
+        }, 500);
+    }
 }
+
 
